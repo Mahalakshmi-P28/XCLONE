0d849ea87c39f834ff66641153219d2a580be2ea
Pilla Mahalakshmi <pillamahalakshmi2004@gmail.com>
2025-02-09 11:31:04 +0530

    1. user routes and controllers added

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req,res,next) => {
     try {
-        const token = req.cookies.jwt;
+        const token = req.cookies.jwt || req.header("Authorization").replace("Bearer ", "");
         if(!token) {
             return res.status(401).json({error: "Unauthorized: No token Provided"});
         }

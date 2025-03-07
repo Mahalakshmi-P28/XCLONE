bbffdf96af5255c4f9c965a3ae82985a65e49805
Pilla Mahalakshmi <pillamahalakshmi2004@gmail.com>
2025-03-07 10:41:38 +0530

    1. notifications and controllers added

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -14,7 +14,7 @@ export const protectRoute = async (req,res,next) => {
         }
         const user = await User.findById(decoded.userId).select("-password");
         if(!user) {
-            return res.status(401).json({error: "Error: User not found"});
+            return res.status(404).json({error: "Error: User not found"});
         }
         req.user = user;
         next();
